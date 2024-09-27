523c18bc245fe4ac6dacad05b63f001469a6c663
Anna Zadiraka <annazadiraka@gmail.com>
2024-09-27 19:53:32 +0300

    add questions

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // Бургер меню
     const burger = document.getElementById('burger'),
     nav = document.getElementById('nav'),
-    menuItems = document.querySelectorAll('.menu__item');
+    menuItems = document.querySelectorAll('.menu__item'),
+    $sectionHero = document.querySelector('.hero'),
+      $sectionAll = document.querySelector('.all');
 
     burger.addEventListener('click', () => {
         burger.classList.toggle('open');
@@ -10,19 +12,51 @@ document.addEventListener("DOMContentLoaded", () => {
         document.body.classList.toggle('scroll-stop');
     })
 
+    // Смена секций при выборе пункта меню и закрытие бургера
     menuItems.forEach(item => {
-        item.addEventListener('click', (event) => {
-            const target = event.target;
+      item.addEventListener('click', (event) => {
+          const target = event.target;
 
-            if (target.closest('.menu__item')) {
-                burger.classList.remove('open');
-                nav.classList.remove('open');
-                document.body.classList.remove('scroll-stop');
-            }
-        })
-    })    
-    
-    // Закрытие по эск   
+          if (target.dataset.section === 'home') {
+            changeHero();
+          } else {
+            changeHero(false)
+            
+          }
+
+          if (target.closest('.menu__item')) {
+              closeBurger()
+          }
+      })
+  })    
+
+  // Смена видимости секций
+  function changeHero(heroVisible = true) {
+    if (heroVisible) {
+      $sectionHero.classList.remove('hidden');
+      $sectionAll.classList.add('hidden');
+      setTimeout(() => {
+      $sectionHero.style.display = 'block'
+    }, 200)
+    } else {
+      $sectionHero.classList.add('hidden');
+      $sectionAll.classList.remove('hidden');
+      
+      // setTimeout(() => {
+        $sectionHero.style.display = 'none'
+        
+      // }, 300)
+      // console.log('111');
+    }
+  }
+
+  function closeBurger () {
+    burger.classList.remove('open');
+    nav.classList.remove('open');
+    document.body.classList.toggle('scroll-stop');
+  }
+
+  // Закрытие по эск   
   window.addEventListener('keydown', function (event) {     
         if (event.key === 'Escape' || event.key === 'Esc') {
           if (burger.classList.contains('open')) {
@@ -105,4 +139,37 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   setInterval(() => createdot(), 500);
+
+  // Аккордеон секции FAQ
+  const $accItemAll = document.querySelectorAll('.questions__item');
+
+  if($accItemAll)
+  $accItemAll.forEach((item, index) => {
+    const $accBtn = item.querySelector('.questions__item--btn');
+
+    if($accBtn)
+    $accBtn.addEventListener('click', () => {
+      item.classList.toggle('open');
+
+        const $body = item.querySelector('.questions__item--body');
+
+        if (item.classList.contains('open')) {
+          $body.style.height = `${$body.scrollHeight}px`
+        } else {
+          $body.style.height = '0px'
+        }
+        closeAcc(index)
+      })
+  })
+
+  function closeAcc(count) {
+    $accItemAll.forEach((item, index) => {
+      if (count !== index) {
+        item.classList.remove('open')
+
+        const $body = item.querySelector('.questions__item--body');
+        $body.style.height = '0px';      
+      }
+    })
+  }
 })
